6b1d9b3c5deda89d6f62bd49bf4d898d87b9b138
bgrove1974 <bgroveben@gmail.com>
2015-05-21 04:10:47 -0700

    Introduction to For loops

---
+++ js_jquery/codecademy/forLoops.js
@@ -0,0 +1,42 @@
+// Example of a for loop:
+
+for (var counter = 1; counter < 11; counter++) {
+	console.log(counter);
+}
+
+// Change where the for loop starts.
+
+for (var i = 5; i < 11; i = i + 1){
+	console.log(i);
+}
+
+// Edit this for loop
+
+for (var i = 4; i < 24; i = i + 1) {
+	console.log(i);
+}
+
+// Increment by 5
+
+for (var i = 5; i < 51; i+=5) {
+	console.log(i);
+}
+
+// Begin at 8 and increment by 12
+
+for (var i = 8 ; i < 121; i+=12) {
+	console.log(i);
+}
+
+// Count down from 10 to 0
+
+for (var i = 10; i >= 0; i--) {
+	console.log(i);
+}
+
+// Start at 100, decrement by five, end at five
+
+for (i = 100; i >= 1; i-=5) {
+    console.log(i);   
+}
+
