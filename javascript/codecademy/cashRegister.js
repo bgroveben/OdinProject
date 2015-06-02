6b8efaade2bd813d197b94bececff14fb535596d
bgrove1974 <bgroveben@gmail.com>
2015-06-02 01:15:20 -0700

    Begin Building a Cash Register

---
+++ javascript/codecademy/cashRegister.js
@@ -0,0 +1,13 @@
+// Building a Cash Register
+
+// You are working for a large supermarket and the cash register has just failed. 
+// The boss is not happy as he can't make any money.
+// To save the day it happens that you let slip to your boss that you know JavaScript and can build a 
+// quick virtual cash register until head office sends support staff.
+
+// Create the object called cashRegister and initialize its total property to 0
+var cashRegister = {
+  total: 0    
+};
+//Using dot notation change the total property
+cashRegister.total = 2.99;
