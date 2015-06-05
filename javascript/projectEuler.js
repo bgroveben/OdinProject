dfa916dc35d6fdf8da8e0e80613a3442f26c9884
bgrove1974 <bgroveben@gmail.com>
2015-06-04 18:57:27 -0700

    Project Euler #3

--- javascript/projectEuler.js
+++ javascript/projectEuler.js
@@ -36,3 +36,49 @@ while (a < 4000000) {
     b = c;
 }
 console.log(total);
+
+
+// #3
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+// Use Seive of Eratosthenes to find all of the prime numbers up to 600851475143 / 2 (300425737571).
+var sieve = function(n) {
+    // Eratosthenes algorithm to find all primes under n
+    var array = [], upperLimit = Math.sqrt(n), output = [];
+    // Make an array from 2 to (n-1)
+    for (var i = 0; i < n; i++) {
+        array.push(true);
+    }
+    // Remove multiples of primes starting from 2, 3, 5,...
+    for (var i = 2; i <= upperLimit; i++) {
+        if (array[i]) {
+            for (var j = i * i; j < n; j += i) {
+                array[j] = false;
+            }
+        }
+    }
+    // All array[i] set to true are primes
+    for (var i = 2; i < n; i++) {
+        if (array[i]) {
+            output.push(i);
+        }
+    }
+    return output;
+};
+// The above code is very thorough, but is not quite what I'm looking for beacuse the run time is so long.
+// In addition to the code above, I would then have to go through each of the resulting numbers to find which ones
+// are prime factors of 600851475143.
+// I used the code below instead.
+var optimusPrime = function(x) {
+    var i = 2;
+    while (i <= x) {
+        if (x % i === 0) {
+            x /= i;
+        }
+        else {
+            i++;
+        }
+    }
+    return i;
+};
+optimusPrime(600851475143);
