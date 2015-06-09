e41a3f9be46150e3910a39efc3731492544fa988
bgrove1974 <bgroveben@gmail.com>
2015-06-09 02:15:20 -0700

    Accordion menu

--- jquery/kryptonExplodes/script.js
+++ jquery/kryptonExplodes/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     $('div').click(function() {
-        $('div').effect('explode');    
+        $('div').effect('explode');
+        // uncomment the following line if you just want to make krypton bounce rapidly
+        // $('div').effect('bounce', {times:3}, 500);
+        // uncomment the following line if you want to make krypton slide into view
+        // $('div').effect('slide');    
     });    
 });
 // For this to work, make sure you have both th jQuery and jQueryUI libraries in script tags in index.html
