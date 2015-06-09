a05144cf9badc28e46a01bc5432137540abdc105
bgrove1974 <bgroveben@gmail.com>
2015-06-09 01:22:38 -0700

    Blow up planet Krypton

---
+++ jquery/kryptonExplodes/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('div').click(function() {
+        $('div').effect('explode');    
+    });    
+});
+// For this to work, make sure you have both th jQuery and jQueryUI libraries in script tags in index.html
