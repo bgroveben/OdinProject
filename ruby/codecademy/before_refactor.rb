8a21f2a65918cc37ef6b78e20ce8abfb17380a38
bgrove1974 <bgroveben@gmail.com>
2015-06-25 05:51:17 -0700

    The Refactor Factory Before

---
+++ ruby/codecademy/before_refactor.rb
@@ -0,0 +1,26 @@
+# This code works, but needs to be refactored.
+# See the results in after_refactor.rb
+
+$VERBOSE = nil    # We'll explain this at the end of the lesson.
+require 'prime'   # This is a module. We'll cover these soon!
+
+def first_n_primes(n)
+
+  unless n.is_a? Integer
+    return "n must be an integer."
+  end
+
+  if n <= 0
+    return "n must be greater than 0."
+  end
+  
+  prime_array = [] if prime_array.nil?
+  
+  prime = Prime.new
+  for num in (1..n)
+    prime_array.push(prime.next)
+  end
+  return prime_array
+end
+
+first_n_primes(10)
