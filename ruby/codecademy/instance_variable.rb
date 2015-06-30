8af60abc342f735141b19915c8a7d6ce965086a0
bgrove1974 <bgroveben@gmail.com>
2015-06-30 01:02:44 -0700

    Instance and Class Variables

---
+++ ruby/codecademy/instance_variable.rb
@@ -0,0 +1,9 @@
+# For example, instance variables belong to a particular object (or "instance").
+
+class Person
+  def initialize(name, age, profession)
+    @name = name
+    @age = age
+    @profession = profession
+  end
+end
