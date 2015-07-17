9543906dd3a183d1be99116bfe8adf941f5b7d53
bgrove1974 <bgroveben@gmail.com>
2015-07-17 00:33:56 -0700

    Begin LTP Chapter 8

---
+++ ruby/LearnToProgram/chapter_08.rb
@@ -0,0 +1,17 @@
+# A Few Things to Try
+
+# Expand upon englishNumber. First, put in thousands. 
+# So it should return 'one thousand' instead of 'ten hundred' and 'ten thousand' instead of 'one hundred hundred'.
+
+
+# Expand upon englishNumber some more. Now put in millions, so you get 'one million' instead of 'one thousand thousand'. 
+# Then try adding billions and trillions. How high can you go?
+
+=begin 
+How about weddingNumber? It should work almost the same as englishNumber, except that it should insert the word "and" all over the place, returning things like 'nineteen hundred and seventy and two', or however wedding invitations are supposed to look. 
+I'd give you more examples, but I don't fully understand it myself. You might need to contact a wedding coordinator to help you.
+=end 
+
+=begin 
+"Ninety-nine bottles of beer..." Using englishNumber and your old program, write out the lyrics to this song the right way this time. Punish your computer: have it start at 9999. (Don't pick a number too large, though, because writing all of that to the screen takes your computer quite a while. A hundred thousand bottles of beer takes some time; and if you pick a million, you'll be punishing yourself as well!
+=end 
