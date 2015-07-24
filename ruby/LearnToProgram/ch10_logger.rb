2ba58b48e91473a344d0761f8f54e3d52779c7ba
bgrove1974 <bgroveben@gmail.com>
2015-07-24 02:25:40 -0700

    Rename and rearrange programs

---
+++ ruby/LearnToProgram/ch10_logger.rb
@@ -0,0 +1,16 @@
+=begin 
+Program Logger. 
+Write a method called log, which takes a string description of a block and, of course, a block. 
+Similar to do_self_importantly, it should puts a string telling that it has started the block, and another string at the 
+end telling you that it has finished the block, and also telling you what the block returned. 
+Test your method by sending it a code block. 
+Inside the block, put another call to log, passing another block to it. (This is called nesting.) 
+In other words, your output should look something like this:
+
+Beginning "outer block"...
+Beginning "some little block"...
+..."some little block" finished, returning:  5
+Beginning "yet another block"...
+..."yet another block" finished, returning:  I like Thai food!
+..."outer block" finished, returning:  false
+=end
---
+++ ruby/LearnToProgram/ch10_logger_improved.rb
@@ -0,0 +1,18 @@
+=begin 
+Better Logger. 
+The output from that last logger was kind of hard to read, and it would just get worse the more you used it. 
+It would be so much easier to read if it indented the lines in the inner blocks. 
+To do this, you'll need to keep track of how deeply nested you are every time the logger wants to write something. 
+To do this, use a global variable, a variable you can see from anywhere in your code. 
+To make a global variable, just precede your variable name with $, like these:  $global, $nestingDepth, and $bigTopPeeWee. 
+In the end, your logger should output code like this:
+
+Beginning "outer block"...
+  Beginning "some little block"...
+    Beginning "teeny-tiny block"...
+    ..."teeny-tiny block" finished, returning:  lots of love
+  ..."some little block" finished, returning:  42
+  Beginning "yet another block"...
+  ..."yet another block" finished, returning:  I love Indian food!
+..."outer block" finished, returning:  true
+=end
