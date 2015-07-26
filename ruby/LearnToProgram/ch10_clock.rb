8e8987941070a77a528761e7237f6ac108121ab8
bgrove1974 <bgroveben@gmail.com>
2015-07-26 00:09:06 -0700

    Fix Grandfather Clock program

--- ruby/LearnToProgram/ch10_clock.rb
+++ ruby/LearnToProgram/ch10_clock.rb
@@ -1,83 +1,56 @@
-=begin 
+=begin
 Grandfather Clock. 
 Write a method which takes a block and calls it once for each hour that has passed today. 
 That way, if I were to pass in the block do puts 'DONG!' end, it would chime (sort of) like a grandfather clock. 
 Test your method out with a few different blocks (including the one I just gave you). 
-Hint: You can use Time.now.hour to get the current hour. 
-However, this returns a number between 0 and 23, so you will have to alter those numbers in order to get ordinary 
-clock-face numbers (1 to 12).
+Hint: You can use  Time.now.hour to get the current hour. 
+However, this returns a number between 0 and 23, so you will have to alter those numbers in order to get ordinary clock-face numbers (1 to 12).
 =end
 
-=begin
+# This is what I came up with, but I'm not sure it is what the author had in mind:
+
 def clock
-  [1,2,3,4,5,6,7,8,9,10,11,12].each do |hour|
-    puts "DONG!" * hour
-  end   
+  hour = Time.now.hour
+  if hour == 0
+    hour = 12
+  elsif hour > 12
+    hour = hour - 12
+  else
+    hour = hour + 1
+  end
+  clock_proc = Proc.new do
+    hour.to_i.times do
+      puts "DONG!"
+    end
+  end
+  clock_proc.call
 end
 
 clock 
 
+puts 
 
-def convert_time(hour)
-  if hour == 0
-    12
-  elsif hour > 12
-    hour = hour - 12
-  else
-    hour
+# This is the code from the answer key:
+
+def clock(some_proc)
+  #convert hours to 12 hour format
+  current_hour = Time.new.hour
+  if current_hour == 0
+    current_hour = current_hour + 12
+  elsif current_hour > 12
+    current_hour = current_hour - 12
   end
-end
-
-p convert_time(0)
-p convert_time(1)
-p convert_time(12)
-p convert_time(13)
-p convert_time(23)
-p convert_time(17)
-
-=end
-
-=begin 
-def clock(hour)
-  if hour == 0
-    12
-  elsif hour > 12
-    hour = hour - 12
-  else
-    hour
+  #call the proc the number of hours passed
+  current_hour.to_i.times do
+  some_proc.call
   end
-  puts "DONG!" * hour
-end
-
-clock(0)
-clock(1)
-clock(12)
-clock(13)
-clock(18)
-clock(20)
-clock(23)
-puts
-t = Time.now.hour
-clock(t)
-puts
-clock(Time.now.hour)
-=end
-
-def clock(clock_proc)
-  hour = Time.now.hour
-  if hour == 0
-    12
-  elsif hour > 12
-    hour = hour - 12
-  else
-    hour
-  end
-  hour.to_i.times do
-    clock_proc.call
-  end
-end
+end 
+# Dong proc
 dong_proc = Proc.new do
-  puts "DONG!"
+  puts 'DONG!'
 end
 
 clock(dong_proc)
+
+# The output is identical, but my method has the proc inside the definition and clock is called with no arguments.
+# The answer key has the proc outside of the method, so the method is called with the proc as an argument.
