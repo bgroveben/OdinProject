655c6375d8d4d55d05df4151c3167cae30ebc2a7
bgrove1974 <bgroveben@gmail.com>
2015-07-26 01:54:17 -0700

    Program Logger

--- ruby/LearnToProgram/ch10_logger.rb
+++ ruby/LearnToProgram/ch10_logger.rb
@@ -14,3 +14,43 @@ Beginning "yet another block"...
 ..."yet another block" finished, returning:  I like Thai food!
 ..."outer block" finished, returning:  false
 =end
+
+def log(block_description, &block)
+  puts 'Beginning "'+block_description+'"...'
+  value = block.call
+  puts '..."'+block_description+'" finished, returning: ' + value.to_s
+end
+
+log('outer block') do
+  log('some little block') do
+    5
+  end
+  log('yet another block') do
+    "I like Thai Food!"
+  end
+  false
+end
+puts
+
+# Here is the code from the answer key. Yeah, I had to cheat. 
+# I couldn't figure out assigning the block.call to a variable.
+# The nesting part confounded me as well. Oh well.
+
+def log(block_description, &block)
+puts 'Beginning "'+block_description+'" . . .' 
+value_returned = block.call
+puts '. . . "'+block_description+'" finished, returning:'
+puts value_returned
+end
+
+log 'outer block' do
+  log 'some little block' do
+    5
+  end
+
+  log 'yet another block' do
+    'I like Thai food!'
+  end
+false
+end
+# When you run the answer key code, value returned is printed on the next line. My code doesn't do that. So there.
